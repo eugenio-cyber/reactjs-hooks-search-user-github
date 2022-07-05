37a5f429d0a616a51f705b5c0e05a55ec3338786
eugenio-cyber <wesleyeugenio3@gmail.com>
2022-07-04 22:59:01 -0300

    seção das informações em andamento

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,18 +1,26 @@
 import Avatar from "../../assets/avatar.png";
+import SectionData from "../SectionData";
 import "./styles.css";
 
 const Main = () => {
   return (
     <main className="main">
       <img className="main__avatar" src={Avatar} alt="Foto do avatar" />
-      <section className="main__">
-        <div className="main__">
-          <h2 className="main__">The Octocat</h2>
-          <a className="main__" href="/"></a>
-          <span className="main__"></span>
+      <section className=" main__content">
+        <div className="main__top">
+          <div className="main__titles">
+            <h2 className="main__title">The Octocat</h2>
+            <a className="main__link" href="/">
+              @octocat
+            </a>
+          </div>
+          <span className="main__joined">Joined 25 Jan 2011</span>
         </div>
-        <div className="main__"></div>
-        <div className="main__"></div>
+        <div className="main__information">
+          <p className="main__bio">This profile has no bio</p>
+
+          <SectionData />
+        </div>
       </section>
     </main>
   );
---
+++ src/components/SectionData/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+const SectionData = () => {
+  return (
+    <div className="data">
+      <div className="data__repos">
+        <span>Repos</span>
+        <b>8</b>
+      </div>
+      <div className="data__followers">
+        <span>Followers</span>
+        <b>3938</b>
+      </div>
+      <div className="data__following">
+        <span>Following</span>
+        <b>9</b>
+      </div>
+    </div>
+  );
+};
+
+export default SectionData;
